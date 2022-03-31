2c4f7c52af60a42faf18c3faec3a32e7a5fad4c3
LeeYunHak <sza0203@naver.com>
2022-03-31 18:01:38 +0900

    Location (expo-location) 사용

--- App.js
+++ App.js
@@ -1,18 +1,38 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import * as Location from "expo-location";
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet, ScrollView, Dimensions } from "react-native";
 // Dimensions 기계 스크린 width 나 height 얻을 수 있음
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
-console.log(SCREEN_WIDTH);
-
 export default function App() {
+  const [city, setCity] = useState("Loading...");
+  const [location, setLocation] = useState();
+  const [ok, setOk] = useState(true);
+  const ask = async () => {
+    // 사용자 위치 동의
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) setOk(false);
+    //사용자의 현재 위치를 1회성으로 전달하도록 요청하고 그 obj 안에 경도 위도 비구조화 할당
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    // Location.reverseGeocodeAsync(location, options) 위치를 우편주소로 지오코딩
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false }
+    );
+    setCity(location[0].city);
+  };
+  useEffect(() => {
+    ask();
+  }, []);
   return (
     // 모든 View는 Flex Container 다 Flex Direction 기본 값은 컬럼이다
     <View style={styles.container}>
       <StatusBar style="dark" />
       <View style={styles.city}>
-        <Text style={styles.cityName}>Seoul</Text>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
       {/* 스크롤 기능 가능하게함 */}
       <ScrollView
